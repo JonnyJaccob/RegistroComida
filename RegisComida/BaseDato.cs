fb056634da484d2e57aecf28ceb85469dbf3b47c
Josue <jaccobjosue@gmail.com>
2021-06-27 22:16:14 -0500

    first commit

---
+++ RegisComida/BaseDato.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace RegisComida
+{
+    public class BaseDato
+    {
+        public MySqlConnection con;
+        //static DataTable dt;
+        public MySqlCommand cmd;
+        public MySqlDataReader dr;
+        string server = "localhost";
+        string database = "restaurante";
+        string user = "root";
+        string password = "";
+        public BaseDato()
+        {
+            con = new MySqlConnection($"Server={server}; Database={database};User=root; Password={password}");
+        }
+        public void Conectar()
+        {
+            //Cadena de conexion
+            try
+            {
+               
+                con.Open();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("" + ex);
+            }
+        }
+        public void Desconectar()
+        {
+            con.Close();
+        }
+        public void ConsultarComando(String consulta)
+        {
+            cmd = new MySqlCommand(consulta, con);
+        }
+        public MySqlCommand ConsultarComando(String consulta,String NoValor)
+        {
+            return new MySqlCommand(consulta, con);
+        }
+        public void Insertar(string sql)
+        {
+            try
+            {
+                MySqlCommand comando = new MySqlCommand(sql, con);
+                comando.ExecuteNonQuery();
+
+            }
+            catch (MySqlException myex)
+            {
+                throw new Exception("" + myex);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("" + ex);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        public void AsignarParametro(String param, MySqlDbType tipo, Object valor) 
+        {
+            cmd.Parameters.Add(param, tipo).Value = valor;
+        }
+        public void CambiarDatabase(string strserver, string strbasedato, string strUsuario, string strContraseña)
+        {
+            server = strserver;
+            database = strbasedato;
+            user = strUsuario;
+            password = strContraseña;
+            con = new MySqlConnection($"Server={server}; Database={database};User=root; Password={password}");
+        }
+    }
+}
---
+++ RegisComida/MainWindow.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using Gtk;
+using MySql.Data.MySqlClient;
+using RegisComida;
+using System.IO;
+
+public partial class MainWindow : Gtk.Window
+{
+    private BaseDato datos = new BaseDato();
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+        datos.CambiarDatabase("localhost", "regis_comida", "root", "");
+        CellRendererPixbuf activeCell = new Gtk.CellRendererPixbuf();
+        CellRendererText utilNameCell = new CellRendererText();
+        TreeViewColumn Columnaid_registro = new TreeViewColumn
+        {
+            Title = "id registro",
+            Expand = false,
+            Sizing = TreeViewColumnSizing.Autosize
+        };
+        //Columnaid_registro.PackStart(activeCell, false);
+        //Columnaid_registro.AddAttribute(activeCell, "pixbuf", 6);
+        Tabla.AppendColumn(Columnaid_registro);
+        Columnaid_registro.PackStart(utilNameCell, true);
+        Columnaid_registro.AddAttribute(utilNameCell, "text", 0);
+        TreeViewColumn Columnaimagenid = new TreeViewColumn
+        {
+            Title = "Imagen",
+            Expand = true,
+            Sizing = TreeViewColumnSizing.Fixed
+        };
+        //Columnaimagenid.PackStart(activeCell, false);
+        //Columnaimagenid.AddAttribute(new Gtk.CellRendererPixbuf(), "pixbuf", 1);
+        Tabla.AppendColumn("Imagen", new Gtk.CellRendererPixbuf(), "pixbuf", 1);
+        TreeViewColumn Columnanombre = new TreeViewColumn
+        {
+            Title = "Nombre",
+            Expand = true,
+            Sizing = TreeViewColumnSizing.Fixed
+        };
+        Columnanombre.PackStart(activeCell, false);
+        Columnanombre.AddAttribute(activeCell, "pixbuf", 6);
+        Tabla.AppendColumn(Columnanombre);
+        Columnanombre.PackStart(utilNameCell, true);
+        Columnanombre.AddAttribute(utilNameCell, "text", 2);
+        TreeViewColumn Columnafecha = new TreeViewColumn
+        {
+            Title = "Fecha",
+            Expand = true,
+            Sizing = TreeViewColumnSizing.Fixed
+        };
+        Columnafecha.PackStart(activeCell, false);
+        Columnafecha.AddAttribute(activeCell, "pixbuf", 6);
+        Tabla.AppendColumn(Columnafecha);
+        Columnafecha.PackStart(utilNameCell, true);
+        Columnafecha.AddAttribute(utilNameCell, "text", 3);
+        //Se inserte
+        registro = new Gtk.ListStore(typeof(string), typeof(Gdk.Pixbuf), typeof(string), typeof(string));
+        Tabla.Model = registro;
+        ShowAll();
+        //Se supone que de esta manera es una ruta relativa
+        string[] paths = { @"/home", "josue-user", "Repos", "ProyectoC","C#","RegisComida","RegisComida","Imagenes", "pizza.jpeg" };
+        string fullPath = System.IO.Path.Combine(paths);
+        //image1.Pixbuf = new Gdk.Pixbuf(@"/home/josue-user/Repos/ProyectoC/C#/RegisComida/RegisComida/Imagenes/pizza.jpeg");
+        image1.Pixbuf = new Gdk.Pixbuf(fullPath);
+    }
+    ListStore registro;
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void btnSalir_Clicked(object sender, EventArgs e)
+    {
+        Application.Quit();
+    }
+    void MensajeError(string x)
+    {
+        MessageDialog mensaje = new MessageDialog(null, DialogFlags.DestroyWithParent, MessageType.Error, ButtonsType.Ok, "Error: " + x);
+        mensaje.Run();
+        mensaje.Destroy();
+    }
+
+    protected void btnInsertar_Clicked(object sender, EventArgs e)
+    {
+    }
+
+    protected void btnMostrar_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Mostrar();
+        }
+        catch (Exception ex)
+        {
+            MensajeError("" + ex);
+        }
+    }
+    private void Mostrar()
+    {
+        string query;
+        MySqlCommand valor;
+        MySqlDataReader reader;
+        try
+        {
+
+            registro.Clear();
+            query = "SELECT id_registro, comida.imagenid, comida.nombre, registro.fecha " +
+            	"FROM registro, comida " +
+            	"WHERE registro.id_comida = comida.id_comida";
+            datos.Conectar();
+            valor = datos.ConsultarComando(query, "");
+            reader = valor.ExecuteReader();
+
+            while (reader.Read())
+            {
+                int id = int.Parse("" + reader["imagenid"]);
+                var imagenes = new Gdk.Pixbuf(ObtenerImagen(id));
+                registro.AppendValues($"{reader["id_registro"].ToString()}", imagenes, $"{reader["nombre"].ToString()}", $"{reader["fecha"].ToString()}");
+            }
+            reader.Close();
+            datos.Desconectar();
+        }catch(MySqlException exx)
+        {
+            throw new Exception("Error" + exx);
+        }
+        catch(Exception ex)
+        {
+            if(ex == null)
+            {
+                MensajeError("Esta vaicio");
+            }
+            string text = ex.ToString();
+            MessageDialog mensaje = new MessageDialog(null, DialogFlags.DestroyWithParent, MessageType.Info, ButtonsType.Ok, $"Error {text}");
+            mensaje.Run();
+            mensaje.Destroy();
+        }
+    }
+    private string ObtenerImagen(int imagenid)
+    {
+        try
+        {
+            string UbicacionImagen = "pizza.jpeg";
+            switch (imagenid)
+            {
+                case 0:
+                    UbicacionImagen = "pizza.jpeg";
+                    break;
+                case 1:
+                    UbicacionImagen = "hamPollo.jpeg";
+                    break;
+                default:
+                    break;
+            }
+            string[] paths = { @"/home", "josue-user", "Repos", "ProyectoC", "C#", "RegisComida", "RegisComida", "Imagenes", UbicacionImagen };
+            string fullPath = System.IO.Path.Combine(paths);
+            return fullPath;
+        }
+        catch (Exception)
+        {
+            throw new Exception("Error al cargar la imagen");
+        }
+
+    }
+}
